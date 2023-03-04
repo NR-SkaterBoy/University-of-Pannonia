f09dbc19622b3abff58bbaf88883a51f9ec63b92
NR-SkaterBoy <nr.rick.dev@gmail.com>
2023-03-04 08:30:51 +0100

    Added c

---
+++ C/Homework_1.c
@@ -0,0 +1,131 @@
+/**
+ * Task 1
+ * Hozz létre kettő előjeles egész változót egy és ketto néven,
+ * valamint két előjel nélküli egész változót harom és negy néven.
+ * (Csak a változó létrehozás kell, a kód többi része nem.)
+ */
+
+int egy;
+int ketto;
+unsigned harom;
+unsigned negy;
+
+/**
+ * Task 2
+ * Hozz létre egy egyszeres pontosságú lebegőpontos változót lebego néven,
+ * egy dupla pontosságú lebegőpontos egész változót dupla néven, valamint egy
+ * karakter típusú változót karakter néven. (Csak a változó létrehozás kell, a kód többi része nem.)
+ */
+
+float lebego;
+double dupla;
+char karakter;
+
+/**
+ * Task 3
+ * Hozz létre egy előjeles egész változót egesz néven, aminek a kezdeti értéke legyen -65
+ * Hozz létre egy dupla pontosságú lebegőpontos egész változót masik néven, aminek a kezdeti értéke legyen 634.4.
+ * Hozz létre egy karakter típusú változót karakter néven, aminek a kezdeti értéke legyen 'c'.
+ */
+
+int egesz = -65;
+double masik = 634.4;
+char karakter = 'c';
+
+/**
+ * Task 4
+ * Egészítsd ki az alábbi kódot úgy, hogy:
+ * az x változó értékének állítsd be az a változó kétszeresét
+ * az y változó értéke legyen a b és a c változók különbsége (b-ből kivonva a c-t)
+ * a z változó értéke legyen az x és az y változók összege
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, x, y, z;
+    scanf("%d %d %d", &a, &b, &c);
+    
+    x = 2 * a;
+    y = b - c;
+    z = x + y;
+
+    printf("%d %d %d\n", x, y, z);
+    return 0;
+}
+
+/**
+ * Task 5
+ * Egészítsd ki az alábbi kódot úgy, hogy:
+ * az x változó értékének állítsd be az b változó felét
+ * az y változó értéke legyen a c változó háromszorosának és a b változó ötszörösének az összege
+ * az y változó értéke legyen a c változó háromszorosának és a b változó ötszörösének az összege
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    double a, b, c, x, y, z;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    
+    x = b / 2;
+    y = (c * 3) + (b * 5);
+    z = y / a;
+
+    printf("%.2lf %.2lf %.2lf\n", x, y, z);
+    return 0;
+}
+
+
+/**
+ * Task 6
+ * Egészítsd ki az alábbi kódot úgy, hogy:
+ * Egészítsd ki az alábbi kódot úgy, hogy:
+ * az y változó értéke legyen a b és a c változók hányadosa (b osztva c-vel)
+ * az y változó értéke legyen a b és a c változók hányadosa (b osztva c-vel)
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, x, y, z;
+    scanf("%d %d %d", &a, &b, &c);
+    
+    x = a % 3;
+    y = b / c;
+    z = b % c;
+
+    printf("%d %d %d\n", x, y, z);
+    return 0;
+}
+
+
+/**
+ * Task 7
+ * Hozd létre a következő változókat a megadott névvel és típussal, illetve kezdeti értékkel, ahol az is kell:
+ * Típus: dupla lebegőpontos, név: y, kezdeti érték: nincs
+ * Típus: dupla lebegőpontos, név: y, kezdeti érték: nincs
+ * Típus: előjeles egész, név: xd, kezdeti érték: nincs
+*/
+
+double y;
+float masik = 0.45;
+int xd;
+
+
+/**
+ * Task 8
+ * Hozd létre a következő változókat a megadott névvel és típussal, illetve kezdeti értékkel, ahol az is kell:
+ * Típus: egyszeres lebegőpontos, név: temp, kezdeti érték: -1.84
+ * Típus: egyszeres lebegőpontos, név: temp, kezdeti érték: -1.84
+ * Típus: dupla lebegőpontos, név: egy, kezdeti érték: nincs
+ * Típus: dupla lebegőpontos, név: egy, kezdeti érték: nincs
+*/
+
+float temp = -1.84;
+unsigned d = 1501;
+double egy;
+int tarol;
---
+++ C/Homework_2.c
@@ -0,0 +1,122 @@
+/**
+ * Task 1
+ * Adottak a következő változók:
+ * Típus: dupla lebegőpontos, név: harom
+ * Típus: dupla lebegőpontos, név: negyedik
+ * Típus: előjeles egész, név: adat
+ * Típus: előjeles egész, név: alma
+ * Típus: egyszeres lebegőpontos, név: az
+ * Típus: előjel nélküli egész, név: korte
+ * Típus: dupla lebegőpontos, név: d
+ */
+
+scanf("%lf\n%lf\n%d\n%d\n%f\n%u\n%lf\n", &harom, &negyedik, &adat, &alma, &az, &korte, &d);
+
+/**
+ * Task 2
+ * Adottak a következő változók:
+ * Típus: előjeles egész, név: d
+ * Típus: karakter, név: nev
+ * Típus: előjel nélküli egész, név: tarol
+ * Típus: dupla lebegőpontos, név: semmi
+ * Típus: karakter, név: a
+ * Típus: előjel nélküli egész, név: valami
+ * Írd meg azt a kódot, amely kiírja a változók értékeit! (Csak a kiírás kell, a kód többi része nem.)
+ * Minden változó külön sorban jelenjen meg. A lebegőpontos értéknél csak 2 tizedesjegy látszódjon.
+ */
+
+printf("%d\n", d);
+printf("%c\n", nev);
+printf("%u\n", tarol);
+printf("%.2lf\n", semmi);
+printf("%c\n", a);
+printf("%u\n", valami);
+
+/**
+ * Task 3
+ * Adottak a következő változók:
+ * Típus: egyszeres lebegőpontos, név: ez
+ * Típus: dupla lebegőpontos, név: harom
+ * Típus: előjel nélküli egész, név: negyedik
+ * Típus: egyszeres lebegőpontos, név: lol
+ * Írd meg azt a kódot, amely beolvassa a változók értékeit! (Csak a beolvasás kell, a kód többi része nem.)
+ */
+
+scanf("%f %lf %u %f", &ez, &harom, &negyedik, &lol);
+
+/**
+ * Task 4
+ * Adottak a következő változók:
+ * Típus: dupla lebegőpontos, név: valami
+ * Típus: előjeles egész, név: ketto
+ * Típus: előjeles egész, név: alma
+ * Típus: dupla lebegőpontos, név: d
+ * Típus: dupla lebegőpontos, név: harom
+ * Típus: karakter, név: ertek
+ * Írd meg azt a kódot, amely kiírja a változók értékeit! (Csak a kiírás kell, a kód többi része nem.)
+ * Minden változó külön sorban jelenjen meg. A lebegőpontos értéknél csak 2 tizedesjegy látszódjon.
+ */
+
+printf("%.2lf\n%d\n%d\n%.2lf\n%.2lf\n%c", valami, ketto, alma, d, harom, ertek);
+
+/**
+ * Task 5
+ * Készíts egy C programot, amely bekér egy számjegyet
+ * (egész szám, a bemenet csak 0-9 érték lesz), és megjeleníti a magyar szöveges megnevezését ékezetek nélkül.
+ * (Ez elágazásos feladat, a printf magától nem tud ilyet.)
+ */
+
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+
+    if (a == 0)
+        printf("nulla");
+    else if (a == 1)
+        printf("egy");
+    else if (a == 2)
+        printf("ketto");
+    else if (a == 3)
+        printf("harom");
+    else if (a == 4)
+        printf("negy");
+    else if (a == 5)
+        printf("ot");
+    else if (a == 6)
+        printf("hat");
+    else if (a == 7)
+        printf("het");
+    else if (a == 8)
+        printf("nyolc");
+    else if (a == 9)
+        printf("kilenc");
+
+    return 0;
+}
+
+/**
+ * Task 6
+ * Egészítsd ki a megadott programot a megfelelő logikai kifejezéssel.
+ * Akkor kell belépni az elágazásba, és végrehajtani az első printf-et, ha az a, b, c, d,
+ * változók közül vagy az a, vagy a b a legnagyobb. Csak az abszolút legnagyobb esetet fogadjuk el, döntetlent nem.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    if (
+        (a > b && a > c && a > d) || (b > a && b > c && b > d))
+    {
+        printf("Az a vagy b az abszolut legnagyobb\n");
+    }
+    else
+    {
+        printf("Nem teljesul\n");
+    }
+    return 0;
+}
